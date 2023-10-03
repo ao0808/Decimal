0283a5217a0284c9e1f645c2282869ce26a1cbb4
tokenpah@student.21-school.ru <tokenpah@student.21-school.ru>
2023-10-03 22:01:21 +0300

    main

---
+++ src/sravnenie/s21_is_greater_or_equal/s21_is_greater_or_equal.c
@@ -0,0 +1,16 @@
+#include "../../s21_decimal.h"
+
+/**
+ * @brief проверяет больше или равно 1 число
+ *
+ * @param value 1 число
+ * @param result 2 число
+ * @return int результат true или false
+ */
+int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
+  int res = TRUE;
+  if (s21_is_less(value_1, value_2) == TRUE) {
+    res = FALSE;
+  }
+  return res;
+}
---
+++ src/sravnenie/s21_is_less_or_equal/s21_is_less_or_equal.c
@@ -0,0 +1,16 @@
+#include "../../s21_decimal.h"
+
+/**
+ * @brief проверяет меньше или = 1 число
+ *
+ * @param value 1 число
+ * @param result 2 число
+ * @return int результат true или false
+ */
+int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
+  int res = TRUE;
+  if (s21_is_greater(value_1, value_2) == TRUE) {
+    res = FALSE;
+  }
+  return res;
+}
